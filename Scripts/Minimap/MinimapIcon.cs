812850b97674aff529963f9b45bedd9e91f6bde6
Mustafa Güngör <34133185+mustafagungormd@users.noreply.github.com>
2025-06-21 19:15:34 +0300

    Delete Scripts directory

--- Scripts/Minimap/MinimapIcon.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public class MinimapIcon : MonoBehaviour
-{
-    public Image Image;
-    public RectTransform RectTransform;
-    public RectTransform IconRectTransform;
-}
--- Scripts/Scene_Manager/SceneManagerScriptableObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SceneTransitionData", menuName = "ScriptableObjects/SceneTransitionData", order = 1)]
-public class SceneTransitionData : ScriptableObject
-{
-    public bool fromScene2 = false;
-}
